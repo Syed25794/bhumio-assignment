c236476612b4785dcd42f34cf34625dc623f730e
Syed25794 <syedgulmohammad25794@gmail.com>
2023-02-16 13:14:51 +0530

    basic application populated with events

---
+++ src/pages/events/[categories]/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const EventsCategoryPage = () => {
+  return (
+    <React.Fragment>
+      <h1>Events in London</h1>
+      <a href="">
+        <img />
+        <h2>Event 1</h2>
+      </a>
+      <a href="">
+        <img />
+        <h2>Event 2</h2>
+      </a>
+      <a href="">
+        <img />
+        <h2>Event 3</h2>
+      </a>
+      <a href="">
+        <img />
+        <h2>Event 4</h2>
+      </a>
+      <a href="">
+        <img />
+        <h2>Event 5</h2>
+      </a>
+    </React.Fragment>
+  );
+};
+
+export default EventsCategoryPage;
---
+++ src/pages/events/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const EventsPage = () => {
+  return (
+    <React.Fragment>
+      <a href="">
+        <img />
+        <h2>Events n Bangaluru</h2>
+      </a>
+      <a href="">
+        <img />
+        <h2>Events in Pune</h2>
+      </a>
+      <a href="">
+        <img />
+        <h2>Events in Delhi</h2>
+      </a>
+    </React.Fragment>
+  );
+};
+
+export default EventsPage;
+
